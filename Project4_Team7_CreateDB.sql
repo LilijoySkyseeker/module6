179b3adfde826327fa626694a7b11c1d1768fc8a
Michael <Napoldin76@gmail.com>
2025-03-27 00:11:20 -0400

    convert to mysql (look at)

--- Project4_Team7_CreateDB.sql
+++ Project4_Team7_CreateDB.sql
@@ -1,53 +1,34 @@
-USE master
-
-go
-
-DECLARE @DatabaseName nvarchar(50)
-SET @DatabaseName = N'Project4_Team7'
-
-DECLARE @SQL varchar(max)
-
-SELECT @SQL = COALESCE(@SQL,'') + 'Kill ' + Convert(varchar, SPId) + ';'
-FROM MASTER..SysProcesses
-WHERE DBId = DB_ID(@DatabaseName) AND SPId <> @@SPId
-
---SELECT @SQL 
-EXEC(@SQL)
-
-IF EXISTS(select * from sys.databases where name='Project4_Team7')
-DROP DATABASE Project4_Team7
-
+DROP DATABASE IF EXISTS Project4_Team7;
 CREATE DATABASE Project4_Team7;
-
 USE Project4_Team7;
 
 CREATE TABLE Users (
-UserID INT IDENTITY(1,1),
-Pass VARCHAR(255) NOT NULL,
-PRIMARY KEY (UserId)
+    UserID INT AUTO_INCREMENT,
+    Pass VARCHAR(255) NOT NULL,
+    PRIMARY KEY (UserID)
 );
 
-CREATE TABLE Classes(
-ClassID INT IDENTITY(1,1),
-ClassName VARCHAR(100),
-ClassNumber INT NOT NULL,
-PRIMARY KEY (ClassID)
+CREATE TABLE Classes (
+    ClassID INT AUTO_INCREMENT,
+    ClassName VARCHAR(100),
+    ClassNumber INT NOT NULL,
+    PRIMARY KEY (ClassID)
 );
 
-CREATE TABLE Login(
-UserID INT,
-ClassID INT,
-Last_Login DATETIME,
-FOREIGN KEY (UserID) REFERENCES Users(UserID),
-FOREIGN KEY (ClassID) REFERENCES Classes(ClassID),
-CONSTRAINT UQ_Login UNIQUE (UserID,ClassID)
+CREATE TABLE Login (
+    UserID INT,
+    ClassID INT,
+    Last_Login DATETIME,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
+    FOREIGN KEY (ClassID) REFERENCES Classes(ClassID) ON DELETE CASCADE,
+    CONSTRAINT UQ_Login UNIQUE (UserID, ClassID)
 );
 
-CREATE TABLE Student(
-UserID INT,
-ClassID INT,
-Enrolled BIT,
-FOREIGN KEY (UserID) REFERENCES Users(UserID),
-FOREIGN KEY (ClassID) REFERENCES Classes(ClassID),
-CONSTRAINT UQ_Student UNIQUE (UserID,ClassID)
+CREATE TABLE Student (
+    UserID INT,
+    ClassID INT,
+    Enrolled BOOLEAN,
+    FOREIGN KEY (UserID) REFERENCES Users(UserID) ON DELETE CASCADE,
+    FOREIGN KEY (ClassID) REFERENCES Classes(ClassID) ON DELETE CASCADE,
+    CONSTRAINT UQ_Student UNIQUE (UserID, ClassID)
 );
